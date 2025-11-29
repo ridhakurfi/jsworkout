040879be0e0e3446ee1f8a15a79ac562b4402180
ridhakurfi <ridhakurfi@gmail.com>
2025-11-29 08:41:36 +0700

    29 Nov Update

--- todolist.js
+++ todolist.js
@@ -2,33 +2,33 @@ const command = process.argv.slice(2);
 const input1 = command[0];
 const input2 = Number(command[1]);
 const input3 = Number(command[2]);
-const filterer = command[0] === undefined || command[1] === undefined;
+const gate = command[0] === undefined || command[1] === undefined;
 const guide = `please input correctly`;
 
 switch (input1) {
   case "add":
-    if (filterer) {
+    if (gate) {
       console.log(guide);
     } else {
       console.log(input2 + input3);
     }
     break;
   case "minus":
-    if (filterer) {
+    if (gate) {
       console.log(guide);
     } else {
       console.log(input2 - input3);
     }
     break;
   case "multiply":
-    if (filterer) {
+    if (gate) {
       console.log(guide);
     } else {
       console.log(input2 * input3);
     }
     break;
   case "divide":
-    if (filterer) {
+    if (gate) {
       console.log(guide);
     } else {
       console.log(input2 / input3);
