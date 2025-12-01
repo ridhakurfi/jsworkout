a577460a9047995ed58a3765eb50159bb1ea4e21
ridhakurfi <ridhakurfi@gmail.com>
2025-12-01 07:35:07 +0700

    December 1 update

--- todolist.js
+++ todolist.js
@@ -1,34 +1,23 @@
-const action1 = process.argv[2];
-const action2 = process.argv[3];
-const action3 = process.argv[4];
+const fs = require("fs");
 
-const rules =
-  action1 === undefined || action2 === undefined || action3 === undefined;
-
-function tutor() {
-  console.log("");
-  console.log("Input is as follows => node abc.js command1 command2 command3");
-  console.log("");
-  console.log("--add command2 command3");
-  console.log("--minus command2 command3");
-  console.log("--multiply command2 command3");
-  console.log("--divide command2 command3");
+class Todo {
+  constructor(id, task) {
+    this.task = task;
+    this.id = id;
+  }
 }
 
-switch (action1) {
-  case "add":
-    rules ? tutor() : console.log(Number(action2) + Number(action3));
-    break;
-  case "minus":
-    rules ? tutor() : console.log(Number(action2) - Number(action3));
-    break;
-  case "multiply":
-    rules ? tutor() : console.log(Number(action2) * Number(action3));
-    break;
-  case "divide":
-    rules ? tutor() : console.log(Number(action2) / Number(action3));
-    break;
-  default:
-    tutor();
-    break;
+function findAll() {
+  let todos = fs.readFileSync("./data.json", "utf8");
+  
+  todos = JSON.parse(todos);
+  console.log(todos);
+
+  let result = todos.map((todo) => {
+    return new Todo(todo.id, todo.task);
+  });
+
+  return result;
 }
+
+findAll()
